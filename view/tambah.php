55041c34f3427886f85330a35a7c31f29e0d3d2a
DESKTOP-BVSEPM9\RIZKY <rama.rizkiye@gmail.com>
2022-03-22 05:13:05 +0700

    Tugas Pemweb Database dan Crud

---
+++ view/tambah.php
@@ -0,0 +1,23 @@
+<?php
+require_once '../model/Peserta.php';
+
+if(adaPermintaanProsesTambah()){
+    $peserta = new Peserta($_POST['email'],$_POST['nama']);
+    $peserta->add();
+    $pesan = "Peserta berhasil ditambah.";
+    kirimKeHalamanUtama($pesan);
+}else {
+    tampilkanFormulirTambahPeserta();
+}
+    
+function adaPermintaanProsesTambah(){
+    if(isset($_POST['email']) && isset($_POST['nama'])){
+        return true;
+    } else return false;
+}
+function tampilkanFormulirTambahPeserta(){
+    require_once 'view/view-tambah-peserta.php';
+}
+function kirimKeHalamanUtama($pesan){
+    header("Location: index.php?pesan=$pesan");
+}
